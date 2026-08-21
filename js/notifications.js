4d0fe9edaf765a7194072ad703d8024ba6e7c3c9
sonnenshine2000-source <sonnenshine2000@gmail.com>
2026-08-21 09:59:41 +0200

    Update fetch URL for send-push function

--- js/notifications.js
+++ js/notifications.js
@@ -173,7 +173,7 @@ export async function sendTestPush() {
   }
 
   const response = await fetch(
-    SUPABASE_URL + '/functions/v1/send-push',
+    SUPABASE_URL + '/functions/v1/send-push-v3',
     {
       method: 'POST',
 
